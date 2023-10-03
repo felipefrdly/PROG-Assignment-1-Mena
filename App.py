22c3d804245a56d0705d4babd5f149455c06a721
felipefrdly <104601475+felipefrdly@users.noreply.github.com>
2023-10-03 18:16:32 -0400

    Refined code
    Made it so all the inputs are put in a while loop until one of 2 options are selected.
    
    Changed the roll function to add attributes at the end of rolls

--- App.py
+++ App.py
@@ -12,23 +12,40 @@ techCheckOne/techCheckTwo:The number the players roll must be higher then presen
 The function uses these inputs to create prompts then calls on Game.py to do the math if it suceeds or fails
 '''
 def ChalCheck(prompt, optionOne, techCheckOne, optionTwo, techCheckTwo):
-    #presents prompt, choices, and asks for user input
-    choice = input(prompt + optionOne + " or " + optionTwo + "? ")
-    
-    #checks if input is option one, returns true if true
-    if choice.lower() == optionOne.lower():
-        return True
+    #this loop loops until the user inputs one of the options
+    while True:
+        #presents prompt, choices, and asks for user input
+        choice = input(prompt + optionOne + " or " + optionTwo + "? ")
+        
+        #checks if input is option one, returns true if true
+        if choice.lower() == optionOne.lower():
+            return True
 
-    #checks if input is option two, returns false if true
-    elif choice.lower() == optionTwo.lower():
-        return False
+        #checks if input is option two, returns false if true
+        elif choice.lower() == optionTwo.lower():
+            return False
+        
+        else:
+            #contines the loop if one of the options isn't selected
+            print("Please select one of the 2 options.")
+            continue
 
 
 #intro
 print("Welcome to Road to Riches! Rules: This is a text based role playing game. You play as one of two bank robbers, you pick where the robbers go and a dice determines your victory.")
 
 #lets user select role
-roleChoice = input("Select a role! (Rouge or Enforcer): ")
+#this loop loops until the user inputs one of the options
+while True:
+    roleChoice = input("Select a role! (Rogue or Enforcer): ")
+    
+    #contines the loop if one of the options isn't selected
+    if roleChoice.lower != "rogue" or "enforcer":
+        print("Please select one of the 2 options.")
+        continue
+
+    else:
+        break
 
 #changes atributes based on what class is selected 
 Game.updateAttributes(roleChoice)
@@ -36,10 +53,4 @@ Game.updateAttributes(roleChoice)
 #debug stuff
 print(Game.firePower, Game.agility, Game.technique, Game.health)
 
-"""
-if ChalCheck("You approach a door way with a guard near it. will you run or hide ", "Run", 1, "Hide", 1) == True:
-    Game.Roll(1, "You sprint past the guard", "You trip and fall")
-
-else:
-    Game.Roll(1, "You hide and remain unoticed", "You make some noise")
-"""
+ChalCheck("W or L: ", "W", 2, "L", 2)
